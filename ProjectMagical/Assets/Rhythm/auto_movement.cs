f36ab2cf16e6c6c19fc6e0758e084e5dceb994eb
YuwuiF <64444139+YuwuiF@users.noreply.github.com>
2023-05-17 09:50:48 -1000

    Basic_Rhythm

---
+++ ProjectMagical/Assets/Rhythm/auto_movement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class auto_movement : MonoBehaviour
+{
+    public float speed = 5.0f;  // movement speed in units per second
+    public Vector3 forwardDirection = Vector3.forward;  // forward direction of movement
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(forwardDirection * speed * Time.deltaTime);
+    }
+}
