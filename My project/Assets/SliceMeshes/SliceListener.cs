5ae2edb37d62356ec7f49c926f2e9b3ae0637059
YuwuiF <64444139+YuwuiF@users.noreply.github.com>
2023-05-31 08:53:50 -1000

    Test Project

---
+++ My project/Assets/SliceMeshes/SliceListener.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SliceListener : MonoBehaviour
+{
+    public Slicer slicer;
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Trigger Enter!");
+        Debug.Log(slicer.isTouched);
+        slicer.isTouched = true;
+        Debug.Log(slicer.isTouched);
+    }
+}
